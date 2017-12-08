5418d12e8ab12b5406ebb1ef740bbd6de1189cf6
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-08 16:19:13 +0530

    Run this class to get Ouput

---
+++ com/pratik/clientTest_Scalar.java
@@ -0,0 +1,30 @@
+package com.pratik.user;
+
+import java.util.List;
+
+import com.pratik.DAO.DAO_Factory;
+import com.pratik.DAO.DAO_Interface;
+import com.pratik.utility.HibernateUtil;
+
+
+
+public class clientTest_Scalar {
+
+	public static void main(String[] args) 
+	{
+		//use dao
+				DAO_Interface dao=DAO_Factory.getInstance();
+				//call getList method
+				List<Object[]> list=dao.getList();
+				//display the list
+							for(Object[]row:list) {
+								for(Object val:row) {
+
+										System.out.println(val);
+									}
+										System.out.println();
+							}
+			//close the session factory
+							HibernateUtil.closeFactory();
+	}//main()
+}//class
